7091ed176ed934d0ca632b3549a2c8392ba14079
zsoltfrks <zsoltfrks@icloud.com>
2025-12-06 12:52:44 +0100

    feat: session timeout handling

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    buildActivity: false,
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
